af082f89b7f5efd642c070029db955d69947050e
SavPepper22 <s.estell.serrano@gmail.com>
2023-06-12 21:51:43 -0500

    tutoring help

--- Starter_Code/Starter_Code/app.py
+++ Starter_Code/Starter_Code/app.py
@@ -1,30 +1,76 @@
 # Import the dependencies.
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
 
 
 
 #################################################
 # Database Setup
 #################################################
-
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
-
+Base = automap_base()
 # reflect the tables
+Base.prepare(autoload_with=engine)
 
-
-# Save references to each table
-
-
-# Create our session (link) from Python to the DB
-
-
+# Save reference to the table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+session = Session(engine)
 #################################################
 # Flask Setup
 #################################################
 
-
+app = Flask(__name__)
 
 
 #################################################
 # Flask Routes
 #################################################
+@app.route("/api/v1.0/precipitation")
+def get_precipitation():
+    """Hi this is the data of precipitation in Hawaii"""
+
+    return jsonify(session.query(Measurement.prcp))
+
+
+@app.route("/")
+def welcome():
+    return (
+        f"Welcome to the Justice League API!<br/>"
+        f"Available Routes:<br/>"
+        f"/api/v1.0/justice-league<br/>"
+        f"/api/v1.0/justice-league/Arthur%20Curry<br/>"
+        f"/api/v1.0/justice-league/Bruce%20Wayne<br/>"
+        f"/api/v1.0/justice-league/Victor%20Stone<br/>"
+        f"/api/v1.0/justice-league/Barry%20Allen<br/>"
+        f"/api/v1.0/justice-league/Hal%20Jordan<br/>"
+        f"/api/v1.0/justice-league/Clark%20Kent/Kal-El<br/>"
+        f"/api/v1.0/justice-league/Princess%20Diana"
+    )
+
+
+@app.route("/api/v1.0/justice-league/<real_name>")
+def justice_league_character(real_name):
+    """Fetch the Justice League character whose real_name matches
+       the path variable supplied by the user, or a 404 if not."""
+
+    canonicalized = real_name.replace(" ", "").lower()
+    for character in justice_league_members:
+        search_term = character["real_name"].replace(" ", "").lower()
+
+        if search_term == canonicalized:
+            return jsonify(character)
+
+    return jsonify({"error": f"Character with real_name {real_name} not found."}), 404
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
