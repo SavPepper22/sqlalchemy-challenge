e1aa1a65fd7045e2c8ccca19ab2718b58d4817bc
SavPepper22 <s.estell.serrano@gmail.com>
2023-06-12 22:26:15 -0500

    Finished final app.py

--- Starter_Code/Starter_Code/app.py
+++ Starter_Code/Starter_Code/app.py
@@ -1,13 +1,11 @@
 # Import the dependencies.
 import numpy as np
-
+import datetime as dt
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
-
 from flask import Flask, jsonify
-
 
 
 #################################################
@@ -34,43 +32,91 @@ app = Flask(__name__)
 #################################################
 # Flask Routes
 #################################################
-@app.route("/api/v1.0/precipitation")
-def get_precipitation():
-    """Hi this is the data of precipitation in Hawaii"""
-
-    return jsonify(session.query(Measurement.prcp))
-
-
 @app.route("/")
 def welcome():
     return (
-        f"Welcome to the Justice League API!<br/>"
+        f"Welcome to the Hawaii Climate Analysis API!<br/>"
         f"Available Routes:<br/>"
-        f"/api/v1.0/justice-league<br/>"
-        f"/api/v1.0/justice-league/Arthur%20Curry<br/>"
-        f"/api/v1.0/justice-league/Bruce%20Wayne<br/>"
-        f"/api/v1.0/justice-league/Victor%20Stone<br/>"
-        f"/api/v1.0/justice-league/Barry%20Allen<br/>"
-        f"/api/v1.0/justice-league/Hal%20Jordan<br/>"
-        f"/api/v1.0/justice-league/Clark%20Kent/Kal-El<br/>"
-        f"/api/v1.0/justice-league/Princess%20Diana"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/temp/start<br/>"
+        f"/api/v1.0/temp/start/end<br/>"
+        f"<p>'start' and 'end' date should be in the format MMDDYYYY.</p>"
     )
 
 
-@app.route("/api/v1.0/justice-league/<real_name>")
-def justice_league_character(real_name):
-    """Fetch the Justice League character whose real_name matches
-       the path variable supplied by the user, or a 404 if not."""
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    """Return the precipitation data for the last year"""
+    # Calculate the date 1 year ago from last date in database (365 days)
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    # Query for the date and precipitation for the last year
+    precipitation = session.query(Measurement.date, Measurement.prcp).\
+        filter(Measurement.date >= prev_year).all()
+    session.close()
+    # Dict with date as the key and prcp as the value
+    precip = {date: prcp for date, prcp in precipitation}
+    return jsonify(precip)
 
-    canonicalized = real_name.replace(" ", "").lower()
-    for character in justice_league_members:
-        search_term = character["real_name"].replace(" ", "").lower()
 
-        if search_term == canonicalized:
-            return jsonify(character)
+@app.route("/api/v1.0/stations")
+def stations():
+    """Return a list of stations."""
+    results = session.query(Station.station).all()
+    session.close()
+    # Unravel results into a 1D array and convert to a list
+    stations = list(np.ravel(results))
+    return jsonify(stations=stations)
 
-    return jsonify({"error": f"Character with real_name {real_name} not found."}), 404
+
+@app.route("/api/v1.0/tobs")
+def temp_monthly():
+    """Return the temperature observations (tobs) for previous year."""
+    # Calculate the date 1 year ago from last date in database
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    # Query the primary station for all tobs from the last year
+    results = session.query(Measurement.tobs).\
+        filter(Measurement.station == 'USC00519281').\
+        filter(Measurement.date >= prev_year).all()
+    session.close()
+    # Unravel results into a 1D array and convert to a list
+    temps = list(np.ravel(results))
+    # Return the results
+    return jsonify(temps=temps)
 
 
-if __name__ == "__main__":
+@app.route("/api/v1.0/temp/<start>")
+@app.route("/api/v1.0/temp/<start>/<end>")
+def stats(start=None, end=None):
+    """Return TMIN, TAVG, TMAX."""
+    # Select statement
+    sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
+    if not end:
+        # start = dt.datetime.strptime(start, "%m/%d/%Y")
+        # # calculate TMIN, TAVG, TMAX for dates greater than start
+        # results = session.query(*sel).\
+        #     filter(Measurement.date >= start).all()
+        # # Unravel results into a 1D array and convert to a list
+        # temps = list(np.ravel(results))
+        # return jsonify(temps)
+        start = dt.datetime.strptime(start, "%m%d%Y")
+        results = session.query(*sel).\
+            filter(Measurement.date >= start).all()
+        session.close()
+        temps = list(np.ravel(results))
+        return jsonify(temps)
+    # calculate TMIN, TAVG, TMAX with start and stop
+    start = dt.datetime.strptime(start, "%m%d%Y")
+    end = dt.datetime.strptime(end, "%m%d%Y")
+    results = session.query(*sel).\
+        filter(Measurement.date >= start).\
+        filter(Measurement.date <= end).all()
+    session.close()
+    # Unravel results into a 1D array and convert to a list
+    temps = list(np.ravel(results))
+    return jsonify(temps=temps)
+
+
+if __name__ == '__main__':
     app.run(debug=True)
